aa9010b642201d689f6862b38ee4e8ef40161a82
Bryan Ford <brndmfrd@gmail.com>
2017-11-08 21:56:42 -0500

    Tag parsing script working in simple case

--- data/parseByTags.py
+++ data/parseByTags.py
@@ -9,8 +9,9 @@ Parse structured log files by their tags.
 Return structured output; rows are ten-minute segments of a day; 
 columns are the counts of each tag type.
 Standalone - Reads cl-argument as input file name.
-'''
+Assumption: The log data passed is all from the same day
 
+'''
 def ParseMap(infile):
     motifRgx = r'^\[[0-9]{4}\-.+?].+?]'
 
@@ -24,57 +25,55 @@ def ParseMap(infile):
     motifRgxC = re.compile(motifRgx)
     lastTime = ''
     
-    with open(infile, 'r') as instream:
-        for line in instream:
-            motif = motifRgxC.search(line)
+    for line in infile:
+        motif = motifRgxC.search(line)
 
-            if (motif):
-                # Regex for the prefix-structure of each row.
-                motifsplit = re.split('\[|\]|\-|T|:|\.', motif.group())
+        if (motif):
+            # Regex for the prefix-structure of each row.
+            motifsplit = re.split('\[|\]|\-|T|:|\.', motif.group())
 
-                year = motifsplit[1]
-                month = motifsplit[2]
-                day = motifsplit[3]
-                hour = motifsplit[4]
-                minute = motifsplit[5]
-                second = motifsplit[6]
-                sev = motifsplit[11]
+            year = motifsplit[1]
+            month = motifsplit[2]
+            day = motifsplit[3]
+            hour = motifsplit[4]
+            minute = motifsplit[5]
+            second = motifsplit[6]
+            sev = motifsplit[11]
+            
+            # map hours to 10 minute segments
+            mapHour = (int(hour) * 6)
+            mapMinute = int(int(minute) / 10)
+            mapTimeseg = (mapHour + mapMinute)
 
-                if len(lastTime) <= 0:
-                    lastTime = (year + month + day)
-                
-                # If the log changes days, we stop parsing.
-                if lastTime != (year + month + day):
-                    break
-                
-                # map hours to 10 minute segments
-                mapHour = (int(hour) * 6)
-                mapMinute = int(int(minute) / 10)
-                mapTimeseg = (mapHour + mapMinute)
+            # Count DEBUG - uses first letter of tag
+            if sev[0] == 'D':
+                tSegments[mapTimeseg][0] += 1 
+                continue
+            # Count VERBOSE - uses first letter of tag
+            if sev[0] == 'V':
+                tSegments[mapTimeseg][1] += 1 
+            # Count INFO - uses first letter of tag
+            if sev[0] == 'I':
+                tSegments[mapTimeseg][2] += 1 
+            # Count WARN - uses first letter of tag
+            if sev[0] == 'W':
+                tSegments[mapTimeseg][3] += 1 
+            # Count ERROR - uses first letter of tag
+            if sev[0] == 'E':
+                tSegments[mapTimeseg][4] += 1 
+            # Count FATAL - uses first letter of tag
+            if sev[0] == 'F':
+                tSegments[mapTimeseg][5] += 1 
+    return tSegments
 
-                # TODO use switch stmnt 
-                # Count DEBUG - uses first letter of tag
-                if sev[0] == 'D':
-                    tSegments[mapTimeseg][0] += 1 
-                # Count VERBOSE - uses first letter of tag
-                if sev[0] == 'V':
-                    tSegments[mapTimeseg][1] += 1 
-                # Count INFO - uses first letter of tag
-                if sev[0] == 'I':
-                    tSegments[mapTimeseg][2] += 1 
-                # Count WARN - uses first letter of tag
-                if sev[0] == 'W':
-                    tSegments[mapTimeseg][3] += 1 
-                # Count ERROR - uses first letter of tag
-                if sev[0] == 'E':
-                    tSegments[mapTimeseg][4] += 1 
-                # Count FATAL - uses first letter of tag
-                if sev[0] == 'F':
-                    tSegments[mapTimeseg][5] += 1 
-
-        return tSegments, lastTime
-
-
+'''
+TODO: replace with unit test
+main should only be called for demonstration or testing purposes
+'''
 if __name__ == "__main__":
-    sampleLogs = ['log1', 'log2']
-    ParseMap(sampleLogs)
+    sampleLogs = ['[2015-09-21T00:16:27.9743119-04:00] [DEBUG] [32 ] [Tgw.Wcs.CustomLogic.Transportation.ServiceAgents.CustomLogicServiceAgent] FinishSendTask for response [Tgw.Wcs.CustomLogic.Transportation.Facade.Contracts.CheckCompletedTransportResponse]', '[2015-09-21T00:16:27.9743119-04:00] [FATAL] [3  ] [Tgw.Wcs.Transportation.Services.PointBehaviorService] LC 00004301557005267111 - After LocationBehavior in PointBehaviorService - /GapFishkill/WA1/CC01/ToAisle3Lift1SI -> /GapFishkill/WA1/CC01/Aisle3Lift1SI - bf0cb0b8-988b-49a6-90f3-0dccf4bed986.', '[2015-09-21T00:16:27.9899406-04:00] [INFO] [50 ] [Tgw.Wcs.Transportation.Services.TransportManager] LC 00004300817225365792 - In FinishTask @ taskManager!']
+
+    tSegments = ParseMap(sampleLogs)
+    for i in tSegments:
+        outst = str(i).ljust(8) + ''.join([str(x).ljust(8) for x in tSegments[i]]) + '\n'
+        print(outst)
