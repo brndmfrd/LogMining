bac72741d4852d5d4bd603eb70076ed013068866
Bryan Ford <brndmfrd@gmail.com>
2017-11-12 22:31:30 -0500

    Encapsulate Trial Pipeline

--- applicationInterface.py
+++ applicationInterface.py
@@ -1,20 +1,35 @@
 #!python3.6
 import applicationConfiguration as appConfig
-
-# Here we provide an interface to the user to interact with each of the trials. 
-
-print(appConfig.trial1)
+import trial_1_pipeline
 
 
+'''
+Single interface for a user to call the separate trials and to re-use data in multiple trials if desired.
+The user selects which trial they want to run and the necessary pipeline is called for that trial.
+e.g. 
+user selects trial 1
+data is read for trial 1 
+...     
+'''
+def StartTrial1():
+    print('StartTrial1')
+    with trial_1_pipeline.Trial1Pipeline() as Trial1:
+        result = Trial1.testprint()
+        print('result ' + str(result))
 
 
 def BeginTrialSelectionLoop():
+    trialDict = {
+        1:StartTrial1
+    }
+	
     keepGoing = 'y'
 
     while(keepGoing.upper() == 'Y'):
-        selection = input('Please select which trial you would like to run [1-9]+')
-        if (int)selection > 0 and (int)selection < 90:
-            
+        selection = input('Please select which trial you would like to run [1-9]+\t\n')
+        if int(selection) > 0 and int(selection) <= len(trialDict):
+            trialDict[int(int(selection))]()
+        keepGoing = input('Run a new Trial? [Y/any]')
 
 
 def Main():
--- trial_1_pipeline.py
+++ trial_1_pipeline.py
@@ -1,4 +1,24 @@
-import data/parseByTags as PBT
+import data.parseByTags as PBT
 
-PBT.testfunction()
+#PBT.testfunction()
 
+class Trial1Pipeline:
+    classValue = 1
+	
+    def __init__(self):
+        self.classValue = 99999
+
+    def __enter__(self):
+        return self
+
+    '''
+    Still trying to understand how to correctly use this method.
+    '''
+    def __exit__(self, exc_type, exc_value, traceback):
+        #print(exc_type)
+        #print(exc_value)
+        isinstance(exc_value, TypeError)
+	
+    def testprint(self):
+        print('it works')
+        return self.classValue
