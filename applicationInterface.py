a5fa5018ac67e5fe1008a1e130af37752317697b
Bryan Ford <brndmfrd@gmail.com>
2017-12-30 19:05:01 -0500

    Code cleanup and trying out atom python linter

--- applicationInterface.py
+++ applicationInterface.py
@@ -1,77 +1,58 @@
-#!python3.6
-import os
-#import applicationConfiguration as appConfig
-import trial_1_pipeline
 
-'''
-Main routine: BeginTrialSelectionLoop
-Extending: 
+"""Python 3.6.3 Anaconda.
+
+Main routine:
+BeginTrialSelectionLoop
+Extending:
     Add new trial in trials directory
-    Add new dictionary reference in BeginTrialSelectionLoop() dictionary 'trialDict'
+    Add new dictionary reference in BeginTrialSelectionLoop() dictionary
+        'trialDict'
     Add new method here referenced by new entry in 'trialDict'
     Add new pipeline script e.g. 'trial_1_pipeline.py' in the same directory
         Pipeline gets data and target trial
         Produces report
         Is disposable
-'''
+"""
+
+import os
+import trial_1_pipeline
 
 applicationLocation = os.path.abspath(os.path.dirname(__file__))
 
-'''
-Single interface for a user to call the separate trials and to re-use data in multiple trials if desired.
-The user selects which trial they want to run and the necessary pipeline is called for that trial.
-e.g. trial_1_pipeline
-user selects trial 1
-data is read for trial 1 
-...     
-'''
+
 def StartTrial1():
+    """Begins trial one's pipeline."""
     print('StartTrial1')
     tempDataRelPos = r'/data/temp/'
-    
+
     inputDataFilePath = applicationLocation + tempDataRelPos
-    
+
     with trial_1_pipeline.Trial1Pipeline(inputDataFilePath) as Trial1:
         result = Trial1.Pipeline()
+        if not result:
+            print('There was a problem processing Trial 1.')
 
 
 def BeginTrialSelectionLoop():
+    """Execute main work loop."""
     # When you add more trials, extend this dictionary!!
     trialDict = {
-        1:StartTrial1
+        1: StartTrial1
     }
-	
+
     keepGoing = 'y'
 
-    while(keepGoing.upper() == 'Y'):
-        selection = input('Please select which trial you would like to run [1-9]+\t\n')
+    while keepGoing.upper() == 'Y':
+        selection = input('Select which trial to run [1-9]+\t\n')
         if int(selection) > 0 and int(selection) <= len(trialDict):
             trialDict[int(int(selection))]()
         keepGoing = input('Run a new Trial? [Y/any]')
 
 
 def Main():
+    """Docstring."""
     BeginTrialSelectionLoop()
 
 
 if __name__ == '__main__':
     Main()
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
--- trial_1_pipeline.py
+++ trial_1_pipeline.py
@@ -1,50 +1,50 @@
+
+"""Python 3.6.3 Anaconda."""
 import data.parseByTags as Pt
 import data.loadOneDay as LoadOneDay
 import pandas as Pd
 
 
+class Trial1Pipeline:
+    """DocString."""
 
-class Trial1Pipeline:	
     def __init__(self, inputDirPath):
+        """DocString."""
         self.inputDataFilePath = inputDirPath
         print('INIT Trial1Pipeline')
-        
-        
+
     def __enter__(self):
+        """DocString."""
         return self
 
-        
-    '''
-    Still trying to understand how to correctly use this method.
-    '''
     def __exit__(self, exc_type, exc_value, traceback):
-        #print(exc_type)
-        #print(exc_value)
+        """DocString."""
+        # print(exc_type)
+        # print(exc_value)
         isinstance(exc_value, TypeError)
-        
-          
+
     def Pipeline(self):
+        """DocString."""
         print('--Start pipeline--')
-        
+
         allTheSegments = []
-        
+
         # Context for our raw data
-        # Raw data access will likely change in the future (DB, HDFS, ETC) 
-        # For now this lets the garbage collector clean up all the data we temporarily hold in memory.
+        # Raw data access will likely change in the future (DB, HDFS, ETC)
+        # Let garbage collector clean up data we temporarily hold in memory.
         with LoadOneDay.LoadOneDay(self.inputDataFilePath) as Loader:
             rawData = Loader.ReadAllFilesInDirectory()
-            print( 'Records read from all files - ' + str(len(rawData)) )
+            print('Records read from all files - ' + str(len(rawData)))
             allTheSegments = Pt.ParseMap(rawData)
-            
-                
-        # Turn our data into a DATAFRAME 
-        # Pretty print our DATAFRAME 
+
+        # Turn our data into a DATAFRAME
+        # Pretty print our DATAFRAME
         df = Pd.DataFrame(allTheSegments)
         df = df.transpose()
-        df.columns = ['DEBUG','VERBOSE','INFO','WARN','ERROR','FATAL',]
+        df.columns = ['DEBUG', 'VERBOSE', 'INFO', 'WARN', 'ERROR', 'FATAL', ]
         with Pd.option_context('display.max_rows', None, 'display.max_columns', 6):
             print(df)
-            
+
         print('--End pipeline--')
-        
+
         return True
